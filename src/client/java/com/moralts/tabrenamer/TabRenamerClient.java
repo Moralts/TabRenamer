e29b9a8cbc7a982e283d6bcb391366932d5731c2
Moralts <instars2233@outlook.com>
2026-03-06 20:16:23 +0800

    first commit

---
+++ src/client/java/com/moralts/tabrenamer/TabRenamerClient.java
@@ -0,0 +1,10 @@
+package com.moralts.tabrenamer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TabRenamerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
